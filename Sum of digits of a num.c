aad5ddac74b4c85dcc550b7b40a04d5a0c0f305b
mokshagnapadimi-collab <mokshagnapadimi@gmail.com>
2025-12-06 12:06:22 +0530

    Add files via upload

---
+++ Sum of digits of a num.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+	int n,rem,sum=0;
+	printf("enter n value");
+	scanf("%d",&n);
+	while(n!=0)
+      {
+      	rem=n%10;
+      	sum=sum+rem;
+      	n=n/10;
+	  }
+	  printf("%d",sum);
+}
