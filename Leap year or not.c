fe12f5c824755a9511f2e43a1270f22c490460bb
mokshagnapadimi-collab <mokshagnapadimi@gmail.com>
2025-12-06 11:45:08 +0530

    Add files via upload

---
+++ Leap year or not.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int year;
+	printf("enter year ");
+	scanf("%d",&year);
+	if((year%400==0)&&(year%4==0)||(year%100!=0))
+	printf("%d is a leap year",year);
+	else
+	printf("not a leap year");
+}
